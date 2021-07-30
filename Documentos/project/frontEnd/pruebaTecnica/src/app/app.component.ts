578cf1d0be529d78c8f16c3d19e70ab8af9c39e7
angel <angelmagosuarez90@gmail.com>
2021-07-30 14:19:03 -0400

    lista

--- Documentos/project/frontEnd/pruebaTecnica/src/app/app.component.ts
+++ Documentos/project/frontEnd/pruebaTecnica/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './component/list/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pruebaTecnica';
+  public listaPersonal:any = []
+
+  constructor(private DataService:DataService){
+
+  }
+  ngOnInit(): void {
+    this.traerData();
+  }
+
+  public traerData(){
+    this.DataService.get('https://my-json-server.typicode.com/HaibuSolutions/prueba-tecnica-sf/user')
+    .subscribe(respuesta => {
+      this.listaPersonal = respuesta;
+      console.log(this.listaPersonal)
+
+    })
+  }
 }
--- Documentos/project/frontEnd/pruebaTecnica/src/app/app.module.ts
+++ Documentos/project/frontEnd/pruebaTecnica/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule} from '@angular/common/http';
+import { ListComponent } from './component/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
